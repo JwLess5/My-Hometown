f7a5d5583143e77b82270986c570b393d97c32a0
JwLess5 <jasonlessenger@gmail>
2018-12-09 15:13:48 -0700

    js prompt

---
+++ capital.js
@@ -0,0 +1,7 @@
+var capital = prompt("What is the capital of Nevada?");
+    if(capital == "Carson City"){
+        alert("That is correct! " + capital + " is the capital of Nevada.")
+    }
+        else{
+            alert("Sorry " + capital + " is not the capital of Nevada.")
+        }
